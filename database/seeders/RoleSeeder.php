5176534b1678fa2d447d269f8c4dbfdf7add471b
Stoyan Haydushki <s.haidushky@abv.bg>
2021-03-03 10:35:42 +0200

    RoleSeeder update

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,27 +16,27 @@ class RoleSeeder extends Seeder
     {
         //Passenger wagons manager role
         $rolePassenger = Role::factory()->create(['name' => 'passenger-manager']);
-        $rolePassenger->permissions()->sync([12]); //TODO: Add permissions
+        $rolePassenger->permissions()->sync([12, 17, 22, 32, 37, 42, 47]); //TODO: Add permissions
         $rolePassenger->save();
 
         //Freight wagons manager role
         $roleFreight = Role::factory()->create(['name' => 'freight-manager']);
-        $roleFreight->permissions()->sync([12]); //TODO: Add permissions
+        $roleFreight->permissions()->sync([12, 27, 32, 37, 42, 47]); //TODO: Add permissions
         $roleFreight->save();
 
         //Tractive units manager role
         $roleLocomotive = Role::factory()->create(['name' => 'locmotive-manager']);
-        $roleLocomotive->permissions()->sync([12]); //TODO: Add permissions
+        $roleLocomotive->permissions()->sync([12, 32, 37, 42, 47]); //TODO: Add permissions
         $roleLocomotive->save();
 
         //Passenger wagons reporter role
         $roleReporter = Role::factory()->create(['name' => 'passenger-reporter']);
-        $roleReporter->permissions()->sync([12]); //TODO: Add permissions
+        $roleReporter->permissions()->sync([12, 22, 42, 47]); //TODO: Add permissions
         $roleReporter->save();
 
         //Administrator role
         $roleAdministrator = Role::factory()->create(['name' => 'administrator']);
-        $roleAdministrator->permissions()->sync([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]); //TODO: Add permissions
+        $roleAdministrator->permissions()->sync(range(1, 51)); //TODO: Add permissions
         $roleAdministrator->save();
     }
 }
